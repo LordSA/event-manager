40b74797cc128ad068bdcffa9556b0766896a1a2
LordSA <dlshacker159@gmail.com>
2026-01-20 12:04:14 +0530

    con nav  added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SmoothScroll from "./components/SmoothScroll";
 import Navbar from "./components/Navbar";
+import ConditionalNavbar from "./components/ConNav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
+        <ConditionalNavbar />
         <SmoothScroll />
         {children}
       </body>
